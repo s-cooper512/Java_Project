9f4cfc1964525d37d4ab636a88b4ce71f9191639
Stephen Cooper <scooper@indeed.com>
2024-03-25 13:54:21 -0500

    Added function that prints a message to console when a search query returns no results

---
+++ src/Util/CheckEmptyQuery.java
@@ -0,0 +1,15 @@
+package Util;
+
+import domain.Word;
+
+import java.util.List;
+
+public class CheckEmptyQuery {
+    public static List<Word> noResults (List<Word> inputList) {
+        if (inputList.size() <= 0) {
+            System.out.println("Your query did not match any results. Please consider adding the entry to the dictionary or searching for another query.");
+        }
+
+        return (inputList);
+    }
+}
--- src/domain/WordFinder.java
+++ src/domain/WordFinder.java
@@ -1,26 +1,28 @@
 package domain;
 
+import Util.CheckEmptyQuery;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class WordFinder {
     public static List<Word> findWordByName (String userInput) {
-        return (WordInitializer.activeDictionary.stream().filter(word -> word.getWordName().equalsIgnoreCase(userInput)).collect(Collectors.toList()));
+        return (CheckEmptyQuery.noResults(WordInitializer.activeDictionary.stream().filter(word -> word.getWordName().equalsIgnoreCase(userInput)).collect(Collectors.toList())));
     }
 
     public static List<Word> findWordByDefinition (String userInput) {
-        return (WordInitializer.activeDictionary.stream().filter(word -> word.getWordDefinition().toLowerCase().contains(userInput.toLowerCase())).collect(Collectors.toList()));
+        return (CheckEmptyQuery.noResults(WordInitializer.activeDictionary.stream().filter(word -> word.getWordDefinition().toLowerCase().contains(userInput.toLowerCase())).collect(Collectors.toList())));
     }
 
     public static List<Word> findWordStartingWith (String userInput) {
-        return (WordInitializer.activeDictionary.stream().filter(word -> word.getWordName().toLowerCase().startsWith(userInput.toLowerCase())).collect(Collectors.toList()));
+        return (CheckEmptyQuery.noResults(WordInitializer.activeDictionary.stream().filter(word -> word.getWordName().toLowerCase().startsWith(userInput.toLowerCase())).collect(Collectors.toList())));
     }
 
     public static List<Word> findWordEndingWith (String userInput) {
-        return (WordInitializer.activeDictionary.stream().filter(word -> word.getWordName().toLowerCase().endsWith(userInput.toLowerCase())).collect(Collectors.toList()));
+        return (CheckEmptyQuery.noResults(WordInitializer.activeDictionary.stream().filter(word -> word.getWordName().toLowerCase().endsWith(userInput.toLowerCase())).collect(Collectors.toList())));
     }
 
     public static List<Word> findWordContaining (String userInput) {
-        return (WordInitializer.activeDictionary.stream().filter(word -> word.getWordName().toLowerCase().contains(userInput.toLowerCase())).collect(Collectors.toList()));
+        return (CheckEmptyQuery.noResults(WordInitializer.activeDictionary.stream().filter(word -> word.getWordName().toLowerCase().contains(userInput.toLowerCase())).collect(Collectors.toList())));
     }
 }
